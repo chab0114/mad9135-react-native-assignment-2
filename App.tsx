b00e28f654ae6e01765683b27faf8bf585a499c0
chab0114 <chab0114@algonquinlive.com>
2025-10-01 22:39:54 -0400

    add babel config and swipe gesture

--- App.tsx
+++ App.tsx
@@ -1,7 +1,12 @@
 // App.tsx
 import React from 'react';
 import HomeScreen from './src/screens/HomeScreen';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function App() {
-  return <HomeScreen />;
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <HomeScreen />
+    </GestureHandlerRootView>
+  );
 }
